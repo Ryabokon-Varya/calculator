bbfbabd54f15796f8f01792b9315ddc83ea94ee5
Ryabokon-Varya <vararabokon673@gmail.com>
2024-09-26 22:19:05 +0300

    Added division, square and average functions to the calculator

--- calculator.cpp
+++ calculator.cpp
@@ -14,3 +14,21 @@ int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
 }
+
+int Calculator::Div(double a, double b)
+{
+	if (b == 0){
+		throw std::invalid_argument("Division by zero");
+	}
+	return a / b;
+}
+
+int Calculator::Square(double a)
+{
+	return a * a;
+}
+
+int Calculator::Average(double a, double b)
+{
+	return (a + b) / 2;
+}
